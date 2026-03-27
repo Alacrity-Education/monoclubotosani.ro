d56e5c279596728cc0463c7cb13394ba88284a24
Sainenco Luchian <lucasainenco2@gmail.com>
2026-03-27 11:23:45 +0100

    Update Logo component to display text instead of images
    Refactor Logo component to remove img elements and display text.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -19,28 +19,13 @@ export const Logo = (props: Props) => {
 
   return (
     /* eslint-disable @next/next/no-img-element */
-    <>
-      <img
-        alt="Payload Logo"
-        loading={loading}
-        fetchPriority={priority}
-        decoding="async"
+      <div 
         className={clsx(
-          "hidden h-10 p-1 sm:inline-block sm:h-20 sm:p-2",
+          "text-lg lg:text-xl font-semibold",
           className,
         )}
-        src="/logo.svg"
-      />
-      <img
-        alt="Payload Logo"
-        width={50}
-        height={50}
-        loading={loading}
-        fetchPriority={priority}
-        decoding="async"
-        className={clsx("block h-10 p-1 sm:hidden", className)}
-        src="/logo-sm.svg"
-      />
-    </>
+       
+      > Monoclu Botosani </div>
+     
   );
 };
